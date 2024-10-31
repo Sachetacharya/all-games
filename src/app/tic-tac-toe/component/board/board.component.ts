a0e954543dede0b8e0e50ce267f07bd525a834f2
Sachet Acharya <sachet.acharya@np.technofex.com>
2024-10-31 11:50:21 +0545

    add human

--- src/app/tic-tac-toe/component/board/board.component.ts
+++ src/app/tic-tac-toe/component/board/board.component.ts
@@ -1,20 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { BoxComponent } from '../box/box.component';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-board',
   standalone: true,
   imports: [CommonModule, BoxComponent],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrl: './board.component.scss',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease-in', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class BoardComponent implements OnInit {
   squares: string[] = [];
   xIsNext: boolean = false;
   winner: string | null = '';
-
-  constructor() { }
+  gameMode: 'human' | 'computer' = 'human';
 
   ngOnInit(): void {
     this.newGame();
@@ -26,17 +34,39 @@ export class BoardComponent implements OnInit {
     this.xIsNext = true;
   }
 
+  setMode(mode: 'human' | 'computer'): void {
+    this.gameMode = mode;
+    this.newGame();
+  }
+
   get player(): string {
     return this.xIsNext ? 'X' : 'O';
   }
 
   makeMove(idx: number): void {
-    if (!this.squares[idx]) {
-      this.squares.splice(idx, 1, this.player);
+    if (!this.squares[idx] && !this.winner) {
+      this.squares[idx] = this.player;
       this.xIsNext = !this.xIsNext;
-    }
+      this.winner = this.calculateWinner();
 
-    this.winner = this.calculateWinner();
+      // Delay the computer's move if in "vs Computer" mode
+      if (this.gameMode === 'computer' && !this.winner && !this.xIsNext) {
+        setTimeout(() => this.computerMove(), 500); // 2-second delay
+      }
+    }
+  }
+
+  computerMove(): void {
+    const availableMoves = this.squares
+      .map((val, idx) => (val === null ? idx : null))
+      .filter((val) => val !== null) as number[];
+
+    if (availableMoves.length > 0) {
+      const move = availableMoves[Math.floor(Math.random() * availableMoves.length)];
+      this.squares[move] = 'O';
+      this.xIsNext = !this.xIsNext;
+      this.winner = this.calculateWinner();
+    }
   }
 
   calculateWinner(): null | string {
@@ -50,8 +80,7 @@ export class BoardComponent implements OnInit {
       [0, 4, 8],
       [2, 4, 6]
     ];
-    for (let i of lines) {
-      const [a, b, c] = i;
+    for (let [a, b, c] of lines) {
       if (
         this.squares[a] &&
         this.squares[a] === this.squares[b] &&
